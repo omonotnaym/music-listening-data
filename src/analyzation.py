c439e548206f3116986fd965cde34c8bb9b2e716
Myantonomo Garcia <myantonomo.cacique@gmail.com>
2026-07-16 19:36:46 -0400

    added the detection of picks and first picks when pulling from the recently played tracks spotify endpoint

--- src/analyzation.py
+++ src/analyzation.py
@@ -1,29 +1,108 @@
-import pandas, json, time, datetime
+import json, time, datetime
+
+fake_set = {
+    "page 1": {
+        "items": [
+            {
+                "track": {"name": "song A", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:28:59.0Z",
+            },
+            {
+                "track": {"name": "song C", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:26:59.0Z",
+            },
+            {
+                "track": {"name": "song B", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:24:59.0Z",
+            },
+            {
+                "track": {"name": "song A", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:22:59.0Z",
+            },
+            {
+                "track": {"name": "song C", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:22:59.0Z",
+            },
+            {
+                "track": {"name": "song B", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:20:59.0Z",
+            },
+            {
+                "track": {"name": "song A", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:18:59.0Z",
+            },
+            {
+                "track": {"name": "song C", "duration_ms": 120000},
+                "played_at": "2026-07-15T12:18:59.0Z",
+            },
+            {
+                "track": {"name": "song B", "duration_ms": 120000},
+                "played_at": "2026-07-15T11:16:59.0Z",
+            },
+            {
+                "track": {"name": "song A", "duration_ms": 120000},
+                "played_at": "2026-07-15T11:15:00.0Z",
+            },
+        ]
+    }
+}
 
 
-def detect_recently_played_track_first_picks(tracks):
+def detect_recently_played_tracks_first_picks(tracks):
     previous_track = None
-    first_picks = 0
+    all_first_picks = dict(total_picks=0)
+    for track in tracks:
+        if not previous_track:
+            previous_track = track
+            all_first_picks["total_picks"] += 1
+            all_first_picks[track["name"]] = 1
+        else:
+            print(int(track["played_at"]))
+            if (
+                int(track["played_at"])
+                - (
+                    int(previous_track["played_at"])
+                    + int(previous_track["duration_in_milliseconds"] / 1000)
+                )
+                >= 3600
+            ):
+                all_first_picks["total_picks"] += 1
+                try:
+                    all_first_picks[track["name"]] += 1
+                except KeyError:
+                    all_first_picks[track["name"]] = 1
+            previous_track = track
+    return all_first_picks
+
+
+def detect_recently_played_tracks_picks(tracks):
+    previous_track = None
+    all_picks = dict(total_picks=0)
     for track in tracks:
         if not previous_track:
             previous_track = track
         else:
-            if int(track["played_at"]) + (int(previous_track["played_at"]) + int(previous_track["duration_in_milliseconds"]))
+            if int(track["played_at"]) < (
+                int(previous_track["played_at"])
+                + int(previous_track["duration_in_milliseconds"] / 1000)
+            ):
+                all_picks["total_picks"] += 1
+                try:
+                    all_picks[track["name"]] += 1
+                except KeyError:
+                    all_picks[track["name"]] = 1
+        previous_track = track
+    return all_picks
 
 
-
-def detect_recently_played_track_picks(tracks):
-    pass
-
-
-def get_recently_played_track_details(pages):
+def get_recently_played_tracks_details(pages):
     tracks = []
     for page in pages.keys():
-        for item in items:
+        for item in pages[page]["items"]:
             track_name = item["track"]["name"]
             track_duration_in_milliseconds = item["track"]["duration_ms"]
             track_played_at = datetime.datetime.fromisoformat(
-                item["track"]["played_at"]
+                item["played_at"]
             ).timestamp()
             # ^firstly create a datetime object using the iso format given by the played_at key by using .fromisoformat()
             # |then covert it into seconds since the epoch using .timestamp() so that it can be used for logic
@@ -37,3 +116,11 @@ def get_recently_played_track_details(pages):
                 ),
             )
     return tracks
+
+
+print(
+    detect_recently_played_tracks_first_picks(
+        get_recently_played_tracks_details(fake_set)
+    )
+)
+print(detect_recently_played_tracks_picks(get_recently_played_tracks_details(fake_set)))
