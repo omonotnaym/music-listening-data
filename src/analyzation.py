475927980514f9ad629b5dd921285dbccb6714f6
Myantonomo Garcia <myantonomo.cacique@gmail.com>
2026-07-17 03:22:27 -0400

    covered the edge cases of when a user has no data, no picks, and when the attempts to get the necessary data for analyzation has failed. finished commenting all functions in main, analyzation, and spotify_requests

--- src/analyzation.py
+++ src/analyzation.py
@@ -2,6 +2,10 @@ import json, time, datetime
 
 
 def detect_recently_played_tracks_first_picks(tracks):
+    """
+    takes the tracks given to it and determines what songs are session starts (aka first picks)
+    a session start is determined as an hour since the last song was fully played
+    """
     previous_track = None
     all_first_picks = dict(total_picks=0)
     for track in tracks:
@@ -28,6 +32,10 @@ def detect_recently_played_tracks_first_picks(tracks):
 
 
 def detect_recently_played_tracks_picks(tracks):
+    """
+    takes the tracks given to it and determines what songs are early cut transitions
+    this is determined when the start time of the song transitioned to is played before the song transitioned from would end
+    """
     previous_track = None
     all_picks = dict(total_picks=0)
     for track in tracks:
@@ -48,38 +56,51 @@ def detect_recently_played_tracks_picks(tracks):
 
 
 def get_recently_played_tracks_details(pages):
+    """
+    takes the pages unfiltered and filters them for the two functions that analyze recently played tracks picks
+    also does some slight formatting with changing iso format strings to time in seconds since the epoch
+    """
     tracks = []
     for page in pages.keys():
         for item in pages[page]["items"]:
-            track_name = item["track"]["name"]
-            track_duration_in_milliseconds = item["track"]["duration_ms"]
-            track_played_at = datetime.datetime.fromisoformat(
-                item["played_at"]
-            ).timestamp()
-            # ^firstly create a datetime object using the iso format given by the played_at key by using .fromisoformat()
-            # |then covert it into seconds since the epoch using .timestamp() so that it can be used for logic
-            # |
-            tracks.insert(
-                0,
-                dict(
-                    name=track_name,
-                    duration_in_milliseconds=track_duration_in_milliseconds,
-                    played_at=track_played_at,
-                ),
-            )
+            try:
+                track_name = item["track"]["name"]
+                track_duration_in_milliseconds = item["track"]["duration_ms"]
+                track_played_at = datetime.datetime.fromisoformat(
+                    item["played_at"]
+                ).timestamp()
+                # ^firstly create a datetime object using the iso format given by the played_at key by using .fromisoformat()
+                # |then covert it into seconds since the epoch using .timestamp() so that it can be used for logic
+                # |
+                tracks.insert(
+                    0,
+                    dict(
+                        name=track_name,
+                        duration_in_milliseconds=track_duration_in_milliseconds,
+                        played_at=track_played_at,
+                    ),
+                )
+            except KeyError:
+                pass
     return tracks
 
 
 def combine_recently_played_tracks_picks(first_picks: dict, picks: dict):
+    """
+    takes the two types of picks that can be detected (as of v1) and combines the numbers to give overall picks
+    and pick percentages per song.
+    """
     aggregation = {}
     total_picks = first_picks["total_picks"] + picks["total_picks"]
     first_picks_set = set([*first_picks.keys()])
     picks_set = set([*picks.keys()])
-    differences = [
+    differences = [  # get all of the differences between the sets
         *first_picks_set.difference(picks_set),
         *picks_set.difference(first_picks_set),
     ]
-    all_picks_keys = first_picks_set.intersection(picks_set).difference({"total_picks"})
+    all_picks_keys = first_picks_set.intersection(picks_set).difference(
+        {"total_picks"}
+    )  # just have the songs as the keys, not total picks
     if all_picks_keys:
         for key in all_picks_keys:
             aggregation[key] = (first_picks[key] + picks[key]) / total_picks * 100
@@ -88,5 +109,4 @@ def combine_recently_played_tracks_picks(first_picks: dict, picks: dict):
             aggregation[difference] = first_picks[difference] / total_picks * 100
         else:
             aggregation[difference] = picks[difference] / total_picks * 100
-    print(aggregation)
     return {"total_picks": total_picks, "picks": {**aggregation}}
